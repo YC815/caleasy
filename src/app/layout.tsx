d414c47873c02f0d74d5c2f629f0421237241c66
YC815 <yushun@chen.zone>
2025-09-23 14:40:26 +0800

    db

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const publishableKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY || "pk_test_c3dlZXBpbmctcmVwdGlsZS05MS5jbGVyay5hY2NvdW50cy5kZXYk";
+  const publishableKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!;
 
   return (
     <ClerkProvider
